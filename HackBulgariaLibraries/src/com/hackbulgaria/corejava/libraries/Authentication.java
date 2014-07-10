a44c44052c199eeeccc28f794025bcbb5bea2472
Svetla <sv.georgieva@gmail.com>
2014-07-10 19:36:33 +0300

    Restoring changes

---
+++ HackBulgariaLibraries/src/com/hackbulgaria/corejava/libraries/Authentication.java
@@ -0,0 +1,15 @@
+package com.hackbulgaria.corejava.libraries;
+
+public class Authentication {
+    private String USERNAME = "sv.georgieva@gmail.com";
+    private String PASS = "Mart0_yankov";
+
+    public String getUsername() {
+        return this.USERNAME;
+    }
+
+    public String getPassword() {
+        return this.PASS;
+    }
+
+}
